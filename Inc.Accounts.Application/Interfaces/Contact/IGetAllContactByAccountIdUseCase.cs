5ee3068919f28de0c75d1c0e275bc218fa28aef3
linh-00 <alinnelauren@hotmail.com>
2025-04-07 21:00:29 -0300

    Criação da UseCase do Contact

---
+++ Inc.Accounts.Application/Interfaces/Contact/IGetAllContactByAccountIdUseCase.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Inc.Accounts.Application.DTO.Response;
+using Inc.Accounts.Shared.Interfaces;
+
+namespace Inc.Accounts.Application.Interfaces.Contact
+{
+    public interface IGetAllContactByAccountIdUseCase: IUseCase<Guid, IEnumerable<ContactResponse>>
+    {
+    }
+}
---
+++ Inc.Accounts.Application/UseCases/Contact/GetAllContactByAccountIdUseCase.cs
@@ -0,0 +1,53 @@
+﻿using AutoMapper;
+using Inc.Accounts.Application.DTO.Response;
+using Inc.Accounts.Application.Interfaces.Address;
+using Inc.Accounts.Application.Interfaces.Contact;
+using Inc.Accounts.Domain.Interfaces;
+using Inc.Accounts.Shared.Constants;
+using Inc.Accounts.Shared.Exceptions.Exceptions;
+using Inc.Accounts.Shared.Interfaces;
+using Inc.Accounts.Shared.Models;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inc.Accounts.Application.UseCases.Contact
+{
+    public class GetAllContactByAccountIdUseCase : IGetAllContactByAccountIdUseCase
+    {
+        public readonly IContactRepository _ContactRepository;
+        public readonly ILogger<GetAllContactByAccountIdUseCase> _Logger;
+        public readonly IMapper _Mapper;
+
+        public GetAllContactByAccountIdUseCase(IContactRepository contactRepository, ILogger<GetAllContactByAccountIdUseCase> logger, IMapper mapper)
+        {
+            _ContactRepository = contactRepository;
+            _Logger = logger;
+            _Mapper = mapper;
+        }
+
+        public async Task<UseCaseResponse<IEnumerable<ContactResponse>>> Execute(Guid request)
+        {
+            var result = new UseCaseResponse<IEnumerable<ContactResponse>>();
+            try
+            {
+                var entity = await _ContactRepository.GetAllByAcountId(request);
+                var mapper = _Mapper.Map<IEnumerable<ContactResponse>>(entity);
+
+                return result.SetSuccess(mapper);
+            }
+
+            catch (RepositoryException ex)
+            {
+                return result.SetNotFound(new ErrorMessage(ErrorCodes.NotFoundError, ex.Message));
+            }
+            catch (Exception ex)
+            {
+                return result.SetInternalServerError(ex.Message);
+            }
+        }
+    }
+}
